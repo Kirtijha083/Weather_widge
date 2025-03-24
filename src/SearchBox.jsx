0192c20f5a07394ccf05ad2106b382838c126cd2
KirtiJha <kirti.jha2025@gmail.com>
2025-03-24 11:20:42 +0530

    added one more method

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -8,11 +8,21 @@ export default function SearchBox() {
 // then added state variable for search-box
 let [city, setCity] = useState("");
 
+let handleChange =  (event) => {
+    setCity(event.target.value);
+}
+
+let handleSubmit =  (event) => {
+    event.preventDefault();
+    console.log(city);
+    setCity("");
+}
+
     return (
         <div className='searchBox'>
             <h3>Search for a weather</h3>
-            <form>
-                <TextField id="city" label="City_Name" variant="outlined" required value={city}/>
+            <form onSubmit={handleSubmit}>
+                <TextField id="city" label="City_Name" variant="outlined" required value={city} onChange={handleChange}/>
                 <br></br><br></br><br></br>
                 <Button variant="contained" type='submit'>Search</Button>
             </form>
